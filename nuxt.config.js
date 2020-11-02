35bd7ec0252b44d86d6bc287b8ca73e3e64fb535
Benjamin Sattler <bsattler.inbox@gmail.com>
2020-11-02 19:27:49 +0100

    General cleanup

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,12 +16,11 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    title: 'Os-Window Web component',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'A web component (also known as custom element) based library which provides window decorations from the most common operating systems.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
